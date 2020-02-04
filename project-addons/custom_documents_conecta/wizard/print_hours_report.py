cc8b2885dcfea9efb3e221d408f4d32b758c5686
javierjcf <javierjcf@gmail.com>
2020-02-04 11:16:50 +0100

    [FIX]custom_documents_conecta: Añadir fechas de periodo al informe de horas

--- project-addons/custom_documents_conecta/wizard/print_hours_report.py
+++ project-addons/custom_documents_conecta/wizard/print_hours_report.py
@@ -13,6 +13,15 @@ class PrintProjectHoursWzd(models.TransientModel):
     date_start = fields.Date('Date Start')
     date_end = fields.Date('Date End')
 
+    @api.model
+    def default_get(self, fields_list):
+        res = super().default_get(fields_list)
+        active_id = self.env.context.get('active_id', False)
+        project = self.env['project.project'].browse(active_id)
+        res['date_start'] = project.period_date_start
+        res['date_end'] = project.period_date_end
+        return res
+
     def print_report(self):
         report_name = 'custom_documents_conecta.report_project_hours'
         active_ids = self._context.get('active_ids')
