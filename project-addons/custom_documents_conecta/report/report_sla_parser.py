46c8f5e5c621803d9fc056c8f5a78c495a5b1a67
javierjcf <javierjcf@gmail.com>
2020-02-24 14:43:28 +0100

    [ADD]custopm_documents_conecta: Report sla

--- project-addons/custom_documents_conecta/report/report_sla_parser.py
+++ project-addons/custom_documents_conecta/report/report_sla_parser.py
@@ -15,18 +15,65 @@ class ReportcontractHours(models.AbstractModel):
         contract_ids = data.get('contract_ids')
         contracts = self.env['contract.contract'].browse(contract_ids)
 
-        domain = [('task_id', '!=', False)]
         date_start = ''
         date_end = ''
+        domain = [('sla_state', '!=', False)]
         if data.get('date_start'):
-            domain.append(('date', '>=', data['date_start']))
+            domain.append(('issue_date', '>=', data['date_start']))
             date_start = fields.Date.from_string(data['date_start']).strftime('%d-%m-%Y')
         if data.get('date_end'):
-            domain.append(('date', '<=', data['date_end']))
+            domain.append(('issue_date', '<=', data['date_end']))
             date_end = fields.Date.from_string(data['date_end']).strftime('%d-%m-%Y')
 
         report_data = {}
-        
+        for contract in contracts:
+            report_data[contract] = {
+                'date_start': date_start,
+                'date_end': date_end,
+                'n_issues': 0,
+                'n_archived': 0,
+                'archived_percent': 0.0,
+                'fail_tasks': []
+            }
+
+            if not contract.project_id:
+                continue
+
+            project = contract.project_id
+            domain.append(('project_id', '=', project.id))
+            tasks = self.env['project.task'].search(domain)
+            if not tasks:
+                continue
+            archived_tasks = tasks.filtered(lambda t: t.sla_state == '1')
+            no_archived_tasks = tasks.filtered(lambda t: t.sla_state != '1')
+
+            num_total = len(tasks)
+            num_archived = len(archived_tasks)
+            percent = (num_archived / num_total) * 100.0
+            report_data[contract].update({
+                'n_issues': num_total,
+                'n_archived': num_archived,
+                'archived_percent': percent
+            })
+
+            for t in no_archived_tasks:
+                response = 'No cumplido'
+                resolution = 'No cumplido'
+                for sla in t.sla_control_ids:
+                    if 'Respuesta' in sla.sla_line_id.sla_id.name and \
+                            sla.sla_state == '1':
+                        response = 'Cumplido'
+                    if 'Resolución' in sla.sla_line_id.sla_id.name and \
+                            sla.sla_state == '1':
+                        response = 'Cumplido'
+
+                dic = {
+                    'id': t.id,
+                    'response': 'No cumplido',
+                    'resolution': 'No cumplido'
+                }
+                report_data[contract]['fail_tasks'].append(dic)
+
       
         return {
             'doc_ids': contracts.ids,
