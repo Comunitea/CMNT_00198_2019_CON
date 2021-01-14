5a233cf4822f181303ad058c95442d5566c79eb9
javierjcf <javierjcf@gmail.com>
2021-01-14 18:56:22 +0100

    [FIX]custom_timecontrol_dates: Correcion hoiras

--- project-addons/custom_timecontrol_dates/models/project.py
+++ project-addons/custom_timecontrol_dates/models/project.py
@@ -15,13 +15,15 @@ class ProjectProject(models.Model):
             remaing_hours = 0.0
             discounted_hours = 0.0
 
-            domain = [('project_id', '=', project.id)]
+            domain = [('project_id', '=', project.id), ('task_id', '!=', False)]
 
             if project.period_date_start:
-                domain += [('date_start', '>=', project.period_date_start)]
+                domain += [
+                    ('date_start', '>=', 
+                     project.period_date_start.strftime('%d-%m-%Y'))]
             if project.period_date_start:
-                domain += [('date_end', '>=', project.period_date_start)]
-
+                domain += [
+                    ('date_end', '<=', project.period_date_end.strftime('%d-%m-%Y'))]
             a_lines = self.env['account.analytic.line'].search(domain)
             hours_quantity = \
                 round(sum(a_lines.mapped('unit_amount')), 2)
