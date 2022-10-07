66bf37085962b96b9377cf5788feadba93ebd77d
Vicente <vicente@comunitea.com>
2022-10-07 14:31:32 +0200

    [ADD] custom_portal_conecta

---
+++ project-addons/custom_portal_conecta/controllers/portal.py
@@ -0,0 +1,219 @@
+##############################################################################
+#    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+#    Copyright (C) 2022 Comunitea Servicios Tecnológicos S.L. All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See thefire
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from collections import OrderedDict
+from operator import itemgetter
+
+from odoo import http, _
+from odoo.http import request
+from odoo.addons.portal.controllers.portal import CustomerPortal
+from odoo.addons.portal.controllers.portal import pager as portal_pager
+from odoo.tools import groupby as groupbyelem
+
+
+class CustomerPortalCustom(CustomerPortal):
+
+    # ------------------------------------------------------------
+    # The filters are set in the original function so adding them
+    # after we call the super makes the controller crash when
+    # the user selects a newly added filter. So we need to overwrite it.
+    # ------------------------------------------------------------
+
+    @http.route(['/my/tasks', '/my/tasks/page/<int:page>'], type='http', auth="user", website=True)
+    def portal_my_tasks(self, page=1, date_begin=None, date_end=None, sortby=None, filterby=None, search=None, search_in='content', groupby='project', **kw):
+        task_stage_opened = int(request.env.ref('custom_portal_conecta.task_stage_opened').sudo().value)
+        task_stage_closed = int(request.env.ref('custom_portal_conecta.task_stage_closed').sudo().value)
+        
+        values = self._prepare_portal_layout_values()
+        searchbar_sortings = {
+            'date': {'label': _('Newest'), 'order': 'create_date desc'},
+            'name': {'label': _('Title'), 'order': 'name'},
+            'stage': {'label': _('Stage'), 'order': 'stage_id'},
+            'update': {'label': _('Last Stage Update'), 'order': 'date_last_stage_update desc'},
+        }
+        searchbar_filters = {
+            'all': {'label': _('All'), 'domain': []},
+            'open': {'label': _('Open'), 'domain': [('stage_id', '=', task_stage_opened)]},
+            'closed': {'label': _('Closed'), 'domain': [('stage_id', '=', task_stage_closed)]},
+            'non-closed': {'label': _('Non closed'), 'domain': [('stage_id', '!=', task_stage_closed)]},
+            'issue': {'label': _('Is issue'), 'domain': [('is_issue', '=', True)]},
+            'non-issue': {'label': _('Is not issue'), 'domain': [('is_issue', '=', False)]},
+        }
+        searchbar_inputs = {
+            'content': {'input': 'content', 'label': _('Search <span class="nolabel"> (in Content)</span>')},
+            'message': {'input': 'message', 'label': _('Search in Messages')},
+            'customer': {'input': 'customer', 'label': _('Search in Customer')},
+            'stage': {'input': 'stage', 'label': _('Search in Stages')},
+            'all': {'input': 'all', 'label': _('Search in All')},
+        }
+        searchbar_groupby = {
+            'none': {'input': 'none', 'label': _('None')},
+            'project': {'input': 'project', 'label': _('Project')},
+        }
+
+        # extends filterby criteria with project the customer has access to
+        projects = request.env['project.project'].search([])
+        for project in projects:
+            searchbar_filters.update({
+                str(project.id): {'label': project.name, 'domain': [('project_id', '=', project.id)]}
+            })
+
+        # extends filterby criteria with project (criteria name is the project id)
+        # Note: portal users can't view projects they don't follow
+        project_groups = request.env['project.task'].read_group([('project_id', 'not in', projects.ids)],
+                                                                ['project_id'], ['project_id'])
+        for group in project_groups:
+            proj_id = group['project_id'][0] if group['project_id'] else False
+            proj_name = group['project_id'][1] if group['project_id'] else _('Others')
+            searchbar_filters.update({
+                str(proj_id): {'label': proj_name, 'domain': [('project_id', '=', proj_id)]}
+            })
+
+        # default sort by value
+        if not sortby:
+            sortby = 'date'
+        order = searchbar_sortings[sortby]['order']
+        # default filter by value
+        if not filterby:
+            filterby = 'non-closed'
+        domain = searchbar_filters[filterby]['domain']
+
+        # archive groups - Default Group By 'create_date'
+        archive_groups = self._get_archive_groups('project.task', domain)
+        if date_begin and date_end:
+            domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
+
+        # search
+        if search and search_in:
+            search_domain = []
+            if search_in in ('content', 'all'):
+                search_domain = OR([search_domain, ['|', ('name', 'ilike', search), ('description', 'ilike', search)]])
+            if search_in in ('customer', 'all'):
+                search_domain = OR([search_domain, [('partner_id', 'ilike', search)]])
+            if search_in in ('message', 'all'):
+                search_domain = OR([search_domain, [('message_ids.body', 'ilike', search)]])
+            if search_in in ('stage', 'all'):
+                search_domain = OR([search_domain, [('stage_id', 'ilike', search)]])
+            domain += search_domain
+
+        # task count
+        task_count = request.env['project.task'].search_count(domain)
+        # pager
+        pager = portal_pager(
+            url="/my/tasks",
+            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby, 'filterby': filterby, 'search_in': search_in, 'search': search, 'groupby': groupby},
+            total=task_count,
+            page=page,
+            step=self._items_per_page
+        )
+        # content according to pager and archive selected
+        if groupby == 'project':
+            order = "project_id, %s" % order  # force sort on project first to group by project in view
+        tasks = request.env['project.task'].search(domain, order=order, limit=self._items_per_page, offset=(page - 1) * self._items_per_page)
+        request.session['my_tasks_history'] = tasks.ids[:100]
+        if groupby == 'project':
+            grouped_tasks = [request.env['project.task'].concat(*g) for k, g in groupbyelem(tasks, itemgetter('project_id'))]
+        else:
+            grouped_tasks = [tasks]
+
+        values.update({
+            'date': date_begin,
+            'date_end': date_end,
+            'grouped_tasks': grouped_tasks,
+            'page_name': 'task',
+            'archive_groups': archive_groups,
+            'default_url': '/my/tasks',
+            'pager': pager,
+            'searchbar_sortings': searchbar_sortings,
+            'searchbar_groupby': searchbar_groupby,
+            'searchbar_inputs': searchbar_inputs,
+            'search_in': search_in,
+            'sortby': sortby,
+            'groupby': groupby,
+            'searchbar_filters': OrderedDict(sorted(searchbar_filters.items(), reverse=True)),
+            'filterby': filterby,
+        })
+        return request.render("project.portal_my_tasks", values)
+    
+    # ------------------------------------------------------------
+    # Overwritting this is better than redo the search and search_count
+    # ------------------------------------------------------------
+
+    @http.route(['/my/invoices', '/my/invoices/page/<int:page>'], type='http', auth="user", website=True)
+    def portal_my_invoices(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
+        values = self._prepare_portal_layout_values()
+        AccountInvoice = request.env['account.invoice']
+
+        domain = []
+
+        searchbar_sortings = {
+            'date': {'label': _('Invoice Date'), 'order': 'date_invoice desc'},
+            'duedate': {'label': _('Due Date'), 'order': 'date_due desc'},
+            'name': {'label': _('Reference'), 'order': 'name desc'},
+            'state': {'label': _('Status'), 'order': 'state'},
+        }
+
+        searchbar_filters = {
+            'all': {'label': _('All'), 'domain': []},
+            'paid': {'label': _('Paid'), 'domain': [('state', '=', 'paid')]},
+            'open': {'label': _('Open'), 'domain': [('state', '=', 'open')]},
+        }
+
+        # default filter by
+        filterby = 'all'
+        if kw and kw.get('filterby'):
+            filterby =  kw.get('filterby')
+        
+        domain = searchbar_filters[filterby]['domain']
+
+        # default sort by order
+        if not sortby:
+            sortby = 'date'
+        order = searchbar_sortings[sortby]['order']
+
+        archive_groups = self._get_archive_groups('account.invoice', domain)
+        if date_begin and date_end:
+            domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
+
+        # count for pager
+        invoice_count = AccountInvoice.search_count(domain)
+        # pager
+        pager = portal_pager(
+            url="/my/invoices",
+            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
+            total=invoice_count,
+            page=page,
+            step=self._items_per_page
+        )
+        # content according to pager and archive selected
+        invoices = AccountInvoice.search(domain, order=order, limit=self._items_per_page, offset=pager['offset'])
+        request.session['my_invoices_history'] = invoices.ids[:100]
+
+        values.update({
+            'date': date_begin,
+            'invoices': invoices,
+            'page_name': 'invoice',
+            'pager': pager,
+            'archive_groups': archive_groups,
+            'default_url': '/my/invoices',
+            'searchbar_sortings': searchbar_sortings,
+            'sortby': sortby,
+            'searchbar_filters': searchbar_filters,
+            'filterby': filterby,
+        })
+        return request.render("account.portal_my_invoices", values)
